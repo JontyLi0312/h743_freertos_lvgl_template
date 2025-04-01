3bb254a5abf659ada4523536d34fe100be239be1
JontyLi <ljt20030312@Outlook.com>
2025-04-02 00:26:06 +0800

    🦄 refactor(user_drivers): move user_drivers path and port lcd_pwm

--- User_Drivers/Inc/lcd_pwm.h
+++ User_Drivers/Inc/lcd_pwm.h
@@ -5,13 +5,13 @@
 #include "stm32h7xx_hal.h"
 #include "lcd_rgb.h" 
  
-#define  LTDC_PWM_PIN								GPIO_PIN_6								// 引脚			
-#define	LTDC_PWM_PORT								GPIOH										// 端口
-#define 	GPIO_LTDC_PWM_CLK_ENABLE        	   __HAL_RCC_GPIOH_CLK_ENABLE()	 	// 时钟
+// #define  LTDC_PWM_PIN								GPIO_PIN_6								// 引脚			
+// #define	LTDC_PWM_PORT								GPIOH										// 端口
+// #define 	GPIO_LTDC_PWM_CLK_ENABLE        	   __HAL_RCC_GPIOH_CLK_ENABLE()	 	// 时钟
 #define  LCD_PWM_AF           					GPIO_AF2_TIM12							// 复用
 	
-#define  LTDC_PWM_TIM								TIM12										// 定时器
-#define  LTDC_PWM_TIM_CLK_ENABLE					__HAL_RCC_TIM12_CLK_ENABLE()		// 时钟
+// #define  LTDC_PWM_TIM								TIM12										// 定时器
+// #define  LTDC_PWM_TIM_CLK_ENABLE					__HAL_RCC_TIM12_CLK_ENABLE()		// 时钟
 #define  LTDC_PWM_TIM_CHANNEL					 	TIM_CHANNEL_1							// 通道
 
 
